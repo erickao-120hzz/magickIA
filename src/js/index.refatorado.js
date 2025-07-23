b1baedd94bcf1ac31e13a4a0bb53a759ae3724d5
Erick Carvalho <erickcarvalho661@hotmail.com>
2025-07-22 21:18:27 -0300

    Primeiro commit - Projeto MagickIA

---
+++ src/js/index.refatorado.js
@@ -0,0 +1,42 @@
+const botaoFiltrar = document.querySelector(".btn-filtrar");
+
+botaoFiltrar.addEventListener("click", function () {
+
+    const categoriaSelecionada = document.querySelector("#categoria").value;
+  const precoMaximoSelecionado = document.querySelector("#preco").value;
+
+  const cartas = document.querySelectorAll(".carta");
+
+  cartas.forEach(function (carta) {
+    const categoriaCarta = carta.dataset.categoria;
+    const precoCarta = carta.dataset.preco;
+
+    let mostrarCarta = true;
+
+    const temFiltroDeCategoria = categoriaSelecionada !== "";
+
+    const cartaNaoBateComFiltroDeCategoria =
+      categoriaSelecionada.toLowerCase() !== categoriaCarta.toLowerCase();
+
+    if (temFiltroDeCategoria && cartaNaoBateComFiltroDeCategoria) {
+      mostrarCarta = false;
+    }
+
+    const temFiltroDePreco = precoMaximoSelecionado !== "";
+    
+    const cartaNaoBateComFiltroDePrecoMaximo =
+      parseFloat(precoCarta) > parseFloat(precoMaximoSelecionado);
+
+    if (temFiltroDePreco && cartaNaoBateComFiltroDePrecoMaximo) {
+      mostrarCarta = false;
+    }
+
+    if (mostrarCarta) {
+      carta.classList.add("mostrar");
+      carta.classList.remove("esconder");
+    } else {
+      carta.classList.remove("mostrar");
+      carta.classList.add("esconder");
+    }
+  });
+});
